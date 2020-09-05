f44ca0f41d2c3f7a36e1e279416bae1fd0b4ffb4
fabriciojs <fajosesouza@gmail.com>
2020-09-05 09:19:52 -0300

    kool pdf microservice - initial commit

---
+++ Dockerfile
@@ -0,0 +1,24 @@
+FROM node:14-alpine
+
+# as suggested at https://github.com/puppeteer/puppeteer/blob/main/docs/troubleshooting.md#running-puppeteer-in-docker
+
+RUN apk add --no-cache \
+      chromium \
+      nss \
+      freetype \
+      freetype-dev \
+      harfbuzz \
+      ca-certificates \
+      ttf-freefont
+
+ENV PUPPETEER_SKIP_CHROMIUM_DOWNLOAD=true \
+    PUPPETEER_EXECUTABLE_PATH=/usr/bin/chromium-browser
+
+# Add user so we don't need --no-sandbox.
+RUN addgroup -S pptruser && adduser -S -g pptruser pptruser \
+    && mkdir -p /home/pptruser/Downloads /app \
+    && chown -R pptruser:pptruser /home/pptruser \
+    && chown -R pptruser:pptruser /app
+
+# Run everything after as non-privileged user.
+USER pptruser
---
+++ pdf-service.js
@@ -0,0 +1,112 @@
+const express = require('express');
+const puppeteer = require('puppeteer');
+const commander = require('commander');
+const path = require('path');
+const fs = require('fs');
+const crypto = require('crypto');
+const app = express();
+
+let port = 3000;
+let calls = {
+    health: 0,
+    fromUrl: 0,
+};
+let isReady = false;
+let browser = null;
+let storagePath = 'storage';
+
+app.get('/health', (req, res) => {
+    calls.health++;
+    console.log('serving /health');
+    deliverJson(res, {isReady, calls}, isReady ? 200 : 503);
+})
+
+app.get('/from-url', async (req, res) => {
+    calls.fromUrl++;
+    const url = req.param('url');
+    console.log('/from-url');
+
+    if (!url) {
+        let msg = 'missing parameter: \'url\'';
+        deliverJson(res, {msg}, 400);
+        return;
+    }
+
+    let pdfFilePath;
+    try {
+        pdfFilePath = await generatePdf(url);
+    } catch (err) {
+        console.log('/from-url: error generating PDF', e);
+        let msg = 'failure generating PDF';
+        deliverJson(res, {msg, err}, 500);
+        return;
+    }
+
+    deliverPdfFile(res, pdfFilePath);
+});
+
+function deliverJson(res, resp, status = 200) {
+    res.status(status).contentType('json').send(resp);
+}
+
+function deliverPdfFile(res, pdfFilePath) {
+    console.log('deliverPdfFile: going to deliver PDF', pdfFilePath);
+    const reader = fs.createReadStream(pdfFilePath);
+    res.setHeader('Content-Length', fs.statSync(pdfFilePath).size);
+    res.setHeader('Content-Type', 'application/pdf');
+    res.setHeader('Content-Disposition', 'inline');
+    reader.pipe(res);
+
+    console.log('deliverPdfFile: going to remove file', pdfFilePath);
+    fs.unlinkSync(pdfFilePath);
+}
+
+async function generatePdf(url) {
+    console.log('generatePdf: browser.newPage');
+    const page = await browser.newPage();
+
+    console.log('generatePdf: emulateMediaType');
+    await page.emulateMediaType('screen');
+
+    console.log('generatePdf: goto', url);
+    await page.goto(url, {
+        waitUntil: 'networkidle2',
+    });
+
+    let filename = crypto.createHash('md5').update(url).digest('hex') + '.pdf';
+    const pdfFolder = path.join(__dirname, storagePath);
+    if (!fs.existsSync(pdfFolder)) {
+        fs.mkdirSync(pdfFolder);
+    }
+    const pdfFilePath = path.join(pdfFolder, filename);
+
+    console.log('generatePdf: generate PDF', pdfFilePath);
+    await page.pdf({
+        path: pdfFilePath,
+        scale: parseFloat(1),
+        format: 'A4',
+        // displayHeaderFooter: false,
+        // printBackground: false,
+        // landscape: false,
+    });
+
+    return pdfFilePath;
+}
+
+app.listen(port, () => {
+    console.log(`Kool PDF service running at port ${port}`);
+    console.log(`Going to start puppeter`);
+
+    (async () => {
+        browser = await puppeteer.launch({
+            args: [
+                '--headless',
+                '--no-sandbox',
+                '--disable-dev-shm-usage',
+            ],
+        });
+
+        console.log('Started puppeteer; setting service state to Ready');
+        isReady = true;
+    })();
+});
